d6c3be4bcea275f937c04b6902457e2d3fae4ea3
Gululu <houzaizai@gmail.com>
2025-11-27 23:12:25 -0800

    update version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         applicationId = "com.gululu.aamediamate"
         minSdk = 29
         targetSdk = 35
-        versionCode = 6
+        versionCode = 8
         versionName = "1.0.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
